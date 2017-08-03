ef944eef303673f78942de15e34651e2317123e1
DAVID DE <daviddecoding@gmail.com>
2017-08-02 20:19:03 -0400

    Add Embeddable Ldap Server for test (#3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,26 +4,29 @@ object Dependencies {
 
   object Versions {
     // compile
-    val unboundidSdk = "4.0.0"
-    val pureConfig = "0.7.2"
+    val apacheLdapApiVersion = "1.0.0-RC2"
+    val pureConfigVersion = "0.7.2"
 
     // test
-    val scalaTest = "3.0.3"
+    val scalaTestVersion = "3.0.3"
+    val apacheDirectoryServiceVersion = "1.5.5"
   }
 
   // compile
-  val unboundidSdk = "com.unboundid" % "unboundid-ldapsdk" % Versions.unboundidSdk
-  val pureConfig = "com.github.pureconfig" %% "pureconfig" % Versions.pureConfig
+  val apacheLdapApi = "org.apache.directory.api" % "api-all" % Versions.apacheLdapApiVersion
+  val pureConfig = "com.github.pureconfig" %% "pureconfig" % Versions.pureConfigVersion
 
   //test
-  val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTestVersion % "test"
+  val apacheDirectoryService = "org.apache.directory.server" % "apacheds-all" % Versions.apacheDirectoryServiceVersion % "test"
 
   val compile = Seq(
-    unboundidSdk
+    apacheLdapApi
   )
 
   val test = Seq(
-    scalaTest
+    scalaTest,
+    apacheDirectoryService
   )
 
   val all = compile ++ test
